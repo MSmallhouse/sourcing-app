9665b2dc6d20deb97a3315ca4d026f78900f78d4
msmallhouse <matthewsmallhouse@gmail.com>
2025-12-05 20:38:24 -0700

    rework stripe onboarding to support updating existing accounts or updating an incomplete onboarding

--- src/app/api/stripe-connect-onboard/route.ts
+++ src/app/api/stripe-connect-onboard/route.ts
@@ -1,44 +1,56 @@
 import { NextResponse } from 'next/server';
 import Stripe from 'stripe';
 import { supabaseAdmin } from '@/lib/supabaseAdmin';
+import { StripeOnboardingStatus } from "@/types/stripe";
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!);
 
 export async function POST(req: Request) {
-  const { userId, email } = await req.json();
+  const { userId, email, stripeOnboardStatus, stripe_account_id } = await req.json();
 
-  // 1. Create or retrieve a connected account for the user
-  const account = await stripe.accounts.create({
-    country: 'US',
-    type: 'express',
-    capabilities: {
-      transfers: {
-        requested: true,
+  // TODO: handle these 3 cases:
+  // acct doesn't exist -> create acct
+  // acct exists but is imcomplete -> get acct and then finish onboarding
+  // acct exists but is complete -> change type under accountLink to be 'account_update' for user to update profile
+
+  let accountId = stripe_account_id;
+
+  if (stripeOnboardStatus === 'not_started') {
+    const account = await stripe.accounts.create({
+      country: 'US',
+      type: 'express',
+      capabilities: {
+        transfers: {
+          requested: true,
+        },
       },
-    },
-    email,
-    metadata: { userId },
-  });
+      email,
+      metadata: { userId },
+    });
+    accountId = account.id;
+  } 
 
   // 2. Create an account link for onboarding
   const accountLink = await stripe.accountLinks.create({
-    account: account.id,
+    account: accountId,
     refresh_url: `${process.env.NEXT_PUBLIC_BASE_URL}/profile`,
     return_url: `${process.env.NEXT_PUBLIC_BASE_URL}/profile`,
     type: 'account_onboarding',
   });
 
-  // add User's stripe id into their profile
-  const { data, error: supabaseError } = await supabaseAdmin
-  .from("profiles")
-  .update({ stripe_account_id: account.id })
-  .eq("id", userId)
-  .select();
+  if (stripeOnboardStatus === 'not_started') {
+    const { data, error: supabaseError } = await supabaseAdmin
+    .from("profiles")
+    .update({ stripe_account_id: accountId })
+    .eq("id", userId)
+    .select();
 
-  if (supabaseError) {
-    console.error("Failed to update stripe_account_id:", supabaseError);
-    return NextResponse.json({ error: "Failed to update profile" }, { status: 500 });
+    if (supabaseError) {
+      console.error("Failed to update stripe_account_id:", supabaseError);
+      return NextResponse.json({ error: "Failed to update profile" }, { status: 500 });
+    }
   }
+
 
   return NextResponse.json({ url: accountLink.url });
 }
