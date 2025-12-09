2b4902f0a38f6ec726acf2e1d32f5955a3520e63
msmallhouse <matthewsmallhouse@gmail.com>
2025-12-08 23:15:14 -0700

    hide top navbar on mobile

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,7 +2,7 @@ import Link from 'next/link'
 
 export default function Navbar() {
   return (
-    <nav className="w-full border-b-1 text-white px-6 py-3 flex items-center justify-between">
+    <nav className="w-full border-b-1 text-white px-6 py-3 items-center justify-between hidden md:flex">
       <div className="text-lg font-bold">Instant Offer Furniture</div>
       <div className="flex space-x-4">
         <Link href="/profile" className="hover:underline">
