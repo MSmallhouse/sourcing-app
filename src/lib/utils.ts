a3cbbb9a15b81d9c82be4967b568b456db0c3bd0
msmallhouse <matthewsmallhouse@gmail.com>
2026-01-08 23:21:15 -0700

    add utility function for checking if the user is the dev (me)

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,8 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export function isDevUser(userId: string | null): boolean {
+  const devUserId = process.env.NEXT_PUBLIC_DEV_USER_ID;
+  return userId === devUserId;
+}
